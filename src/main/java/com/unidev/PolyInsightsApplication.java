7187e63fba09d0207508416b60fbfec6f5b3d708
Denis O <denis.o@linux.com>
2017-02-11 13:09:53 +0200

    Basic project layout

---
+++ src/main/java/com/unidev/PolyInsightsApplication.java
@@ -0,0 +1,12 @@
+package com.unidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyInsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyInsightsApplication.class, args);
+	}
+}
