8418be3e755015eac4303d5a323c9c121cf61040
Denis O <denis.o@linux.com>
2017-02-20 21:37:34 +0200

    Added tenant model

---
+++ poly-insights/src/main/java/com/unidev/service/model/Tenant.java
@@ -0,0 +1,40 @@
+package com.unidev.service.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Map;
+
+@Document(collection = "tenant")
+public class Tenant {
+
+    @Id
+    private String tenant;
+
+    private Map<String, InsightType> types;
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Tenant{");
+        sb.append("tenant='").append(tenant).append('\'');
+        sb.append(", types=").append(types);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public String getTenant() {
+        return tenant;
+    }
+
+    public void setTenant(String tenant) {
+        this.tenant = tenant;
+    }
+
+    public Map<String, InsightType> getTypes() {
+        return types;
+    }
+
+    public void setTypes(Map<String, InsightType> types) {
+        this.types = types;
+    }
+}
