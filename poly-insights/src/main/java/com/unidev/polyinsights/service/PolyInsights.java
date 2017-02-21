dc41ba9f4e99d29b8386461584720a94b2adb76f
Denis O <denis.o@linux.com>
2017-02-21 14:02:42 +0200

    Added insight record

---
+++ poly-insights/src/main/java/com/unidev/polyinsights/service/PolyInsights.java
@@ -0,0 +1,17 @@
+package com.unidev.polyinsights.service;
+
+
+import com.unidev.polyinsights.model.InsightRecord;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service for doing operations on insights
+ */
+@Service
+public class PolyInsights {
+
+    public void logInsight(InsightRecord insightRecord) {
+
+    }
+
+}
