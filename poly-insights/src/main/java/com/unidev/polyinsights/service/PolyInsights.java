40413a1685f5c5f436e66f5eb544d07d855f862a
Denis O <denis.o@linux.com>
2017-02-21 15:25:59 +0200

    Added poly insights validation chain

--- poly-insights/src/main/java/com/unidev/polyinsights/service/PolyInsights.java
+++ poly-insights/src/main/java/com/unidev/polyinsights/service/PolyInsights.java
@@ -1,8 +1,15 @@
 package com.unidev.polyinsights.service;
 
 
-import com.unidev.polyinsights.model.InsightRecord;
+import com.unidev.polyinsights.model.InsightRequest;
+import com.unidev.polyinsights.model.InsightType;
+import com.unidev.polyinsights.model.Tenant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * Service for doing operations on insights
@@ -10,7 +17,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class PolyInsights {
 
-    public void logInsight(InsightRecord insightRecord) {
+    private static Logger LOG = LoggerFactory.getLogger(PolyInsights.class);
+
+    @Autowired
+    private TenantDAO tenantDAO;
+
+    /**
+     * Log insight for storage
+     * @param insightRecord
+     */
+    public void logInsight(InsightRequest insightRecord) {
+
+        Tenant tenant = tenantDAO.findOne(insightRecord.getTenant());
+        if (tenant == null) {
+            LOG.warn("No matched tenant for insight {}", insightRecord);
+            throw new InsightNotAccepted("Tenant not found");
+        }
+
+        Optional<InsightType> optionalInsightType = tenant.fetchInsight(insightRecord.getKey());
+        if (!optionalInsightType.isPresent()) {
+            LOG.warn("Insight type not accepted {}", insightRecord);
+            throw new InsightNotAccepted("Insight type not accepted");
+        }
+
+        InsightType insightType = optionalInsightType.get();
+
+        if (!insightType.getValues().contains(insightRecord.getValue())) {
+            LOG.warn("Insight value not accepted {}", insightRecord);
+            throw new InsightNotAccepted("Insight value not accepted");
+        }
+
+
+
+
+
 
     }
 
