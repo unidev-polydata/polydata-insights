1a7ae1b2c103b4120281aa76d5fd7d99a1c9dd51
Denis O <denis.o@linux.com>
2017-02-22 12:15:13 +0200

    Added field for same insight interval

--- poly-insights/src/main/java/com/unidev/polyinsights/model/InsightType.java
+++ poly-insights/src/main/java/com/unidev/polyinsights/model/InsightType.java
@@ -11,6 +11,7 @@ public class InsightType {
     private String name;
     private Set<String> values;
     private long interval;
+    private long sameInsightInterval;
 
     public String getName() {
         return name;
@@ -36,12 +37,21 @@ public class InsightType {
         this.interval = interval;
     }
 
+    public long getSameInsightInterval() {
+        return sameInsightInterval;
+    }
+
+    public void setSameInsightInterval(long sameInsightInterval) {
+        this.sameInsightInterval = sameInsightInterval;
+    }
+
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("InsightType{");
         sb.append("name='").append(name).append('\'');
         sb.append(", values=").append(values);
         sb.append(", interval=").append(interval);
+        sb.append(", sameInsightInterval=").append(sameInsightInterval);
         sb.append('}');
         return sb.toString();
     }
