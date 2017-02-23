f64439533ab6900514f570cfac060bfcc4307c1c
Denis O <denis.o@linux.com>
2017-02-23 11:46:32 +0200

    Added time interval in ms

--- poly-insights/src/main/java/com/unidev/polyinsights/model/TimeInterval.java
+++ poly-insights/src/main/java/com/unidev/polyinsights/model/TimeInterval.java
@@ -1,10 +1,27 @@
 package com.unidev.polyinsights.model;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Time interval used for queries
  */
 public enum TimeInterval {
 
-    DAY, WEEK, MONTH, YEAR
+    DAY(TimeUnit.DAYS.toMillis(1)), WEEK(TimeUnit.DAYS.toMillis(7)), MONTH(TimeUnit.DAYS.toMillis(30)), YEAR(TimeUnit.DAYS.toMillis(365));
+
+    public Date fetchDateFrom(Date date) {
+        return new Date(date.getTime() - interval);
+    }
+
+    private long interval;
+
+    TimeInterval(long interval) {
+        this.interval = interval;
+    }
+
+    public long getInterval() {
+        return interval;
+    }
 
 }
