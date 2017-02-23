39f6eeed7a538117e71d6712c646abc21ce25564
Denis O <denis.o@linux.com>
2017-02-23 14:42:45 +0200

    Added key property for insight query object

--- poly-insights/src/main/java/com/unidev/polyinsights/model/InsightQuery.java
+++ poly-insights/src/main/java/com/unidev/polyinsights/model/InsightQuery.java
@@ -9,14 +9,25 @@ public class InsightQuery {
     private TimeInterval interval;
     private String insight;
 
+    private String key;
+
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("InsightQuery{");
         sb.append("tenant='").append(tenant).append('\'');
         sb.append(", interval=").append(interval);
         sb.append(", insight='").append(insight).append('\'');
+        sb.append(", key='").append(key).append('\'');
         sb.append('}');
         return sb.toString();
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
     }
 
     public String getTenant() {
