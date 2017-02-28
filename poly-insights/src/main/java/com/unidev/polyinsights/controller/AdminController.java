316fcf3baa49a0e0cfa2adc2d43d9454907210f7
Denis O <denis.o@linux.com>
2017-02-28 19:12:54 +0200

    Basic admin controller class

---
+++ poly-insights/src/main/java/com/unidev/polyinsights/controller/AdminController.java
@@ -0,0 +1,34 @@
+package com.unidev.polyinsights.controller;
+
+import com.unidev.polyinsights.service.TenantDAO;
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Controller for management of insights
+ */
+@SpringUI
+@Theme("valo")
+public class AdminController extends UI {
+
+
+    @Autowired
+    private TenantDAO tenantDAO;
+
+    @Override
+    protected void init(VaadinRequest request) {
+        VerticalLayout content = new VerticalLayout();
+        setContent(content);
+
+        HorizontalLayout titleBar = new HorizontalLayout();
+        content.addComponent(titleBar);
+
+
+
+    }
+}
