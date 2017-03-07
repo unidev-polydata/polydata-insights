5119a1c8580ca48c07752bff7d96d4e80d8db431
Denis O <denis.o@linux.com>
2017-03-07 22:24:06 +0200

    Enabled swagger api

--- poly-insights/src/main/java/com/unidev/polyinsights/Application.java
+++ poly-insights/src/main/java/com/unidev/polyinsights/Application.java
@@ -9,6 +9,11 @@ import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -16,6 +21,7 @@ import javax.servlet.ServletException;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableMongoRepositories
+@EnableSwagger2
 public class Application implements ServletContextInitializer {
 
 	public static final String VERSION = "0.0.1";
@@ -33,6 +39,15 @@ public class Application implements ServletContextInitializer {
     public void onStartup(ServletContext servletContext) throws ServletException {
         servletContext.addServlet("JmxMiniConsoleServlet", MiniConsoleServlet.class).addMapping("/jmx/*");
     }
+
+	@Bean
+	public Docket apiDocs() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 
 }
 
