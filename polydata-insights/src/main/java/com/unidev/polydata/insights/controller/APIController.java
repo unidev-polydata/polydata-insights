1e78656e2fdb40e262a8528efd9432d8a0ab406c
Denis O <denis.o@linux.com>
2017-07-23 14:47:34 +0300

    Updated insights controller configuration

--- polydata-insights/src/main/java/com/unidev/polydata/insights/controller/APIController.java
+++ polydata-insights/src/main/java/com/unidev/polydata/insights/controller/APIController.java
@@ -6,17 +6,18 @@ import com.unidev.polydata.insights.model.HateoasResponse;
 import com.unidev.polydata.insights.model.InsightQuery;
 import com.unidev.polydata.insights.model.InsightRequest;
 import com.unidev.polydata.insights.service.PolyInsights;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * API controller
  */
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class APIController {
 
     @Autowired
@@ -25,7 +26,7 @@ public class APIController {
     @Autowired
     private WebUtils webUtils;
 
-    @PostMapping("/api/insight")
+    @PostMapping("insight")
     public void insight(@RequestBody InsightRequest insightRequest, HttpServletRequest httpServletRequest) {
         String clientIp = webUtils.getClientIp(httpServletRequest);
         String clientId = httpServletRequest.getHeader("ClientId");
@@ -34,17 +35,17 @@ public class APIController {
         polyInsights.logInsight(insightRequest, clientIp + ":" + clientId, basicPoly);
     }
 
-    @PostMapping("/api/insight/value/sum")
+    @PostMapping("insight/value/sum")
     public HateoasResponse listTopKeysByValueSum(@RequestBody InsightQuery insightQuery) {
         return HateoasResponse.hateoasResponse(polyInsights.listTopKeysByValueSum(insightQuery));
     }
 
-    @PostMapping("/api/insight/value/average")
+    @PostMapping("insight/value/average")
     public HateoasResponse listTopKeysByAverageValue(@RequestBody InsightQuery insightQuery) {
         return HateoasResponse.hateoasResponse(polyInsights.listTopKeysByAverageValue(insightQuery));
     }
 
-    @PostMapping("/api/insight/key")
+    @PostMapping("insight/key")
     public HateoasResponse fetchInsightStatsByKey(@RequestBody InsightQuery insightQuery) {
         return HateoasResponse.hateoasResponse(polyInsights.fetchInsightStatsByKey(insightQuery));
     }
